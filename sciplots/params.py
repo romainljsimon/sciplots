7d978df06af17fed56064938cce74aae1d165062
Romain Simon <93144534+romainljsimon@users.noreply.github.com>
2025-01-22 15:22:23 +0100

    Bug correction in update_params)

--- sciplots/params.py
+++ sciplots/params.py
@@ -5,14 +5,12 @@ import colorsys
 
 
 def update_params(fig_width_pt=400, golden_mean=0.5):
-    fig_width_pt = 400     # Get this from LaTeX using \the\columnwidth
     inches_per_pt = 1.0/72.27               # Convert pt to inch
-    golden_mean = 0.4     # Aesthetic ratio
     fig_width = fig_width_pt*inches_per_pt  # width in inches
     fig_height = fig_width*golden_mean      # height in inches
     fig_size =  [fig_width, fig_height]
 
-    params = {#'backend': 'macosx',
+    params = {'backend': 'pdf',
             'axes.labelsize': 12,  
             'legend.fontsize': 7,
             'xtick.labelsize': 8,
