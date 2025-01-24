5ffdaad9558e4c6ef9c97b3e1a159c9751f67784
Romain Simon <93144534+romainljsimon@users.noreply.github.com>
2025-01-24 18:18:26 +0100

    New feature to get custom cmap

--- sciplots/params.py
+++ sciplots/params.py
@@ -2,6 +2,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.colors as mc
 import colorsys
+import matplotlib.colors as mcol
+import matplotlib.cm as cm
 
 
 def update_params(fig_width_pt=400, golden_mean=0.5):
@@ -28,3 +30,11 @@ def adjust_lightness(color, amount=0.5):
         c = color
     c = colorsys.rgb_to_hls(*mc.to_rgb(c))
     return colorsys.hls_to_rgb(c[0], np.max([0, np.min([1, amount * c[1]])]), c[2])
+
+def get_rb(data):
+    # Make a user-defined colormap.
+    cm1 = mcol.LinearSegmentedColormap.from_list("MyCmapName",["b","r"])
+    cnorm = mcol.Normalize(vmin=min(data),vmax=max(data))
+    cpick = cm.ScalarMappable(norm=cnorm,cmap=cm1)
+    cpick.set_array([])
+    return cpick.to_rgba(data)
