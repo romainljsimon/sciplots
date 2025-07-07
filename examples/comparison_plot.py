23fe44b1a29691770e6c444e6d3b20bcb5139f84
Romain Simon <93144534+romainljsimon@users.noreply.github.com>
2025-07-07 14:50:57 +0200

    Add labels to example

--- examples/comparison_plot.py
+++ examples/comparison_plot.py
@@ -18,8 +18,9 @@ plt.show()
 #spl.update_params(fig_width_pt=300, golden_mean=1.0, backend='pdf')
 fig, ax = plt.subplots()
 ax.plot(x, y1, label=r'$y = x^2$', color='blue')
-spl.Axes(ax, xlim=(0.1, 100), ylim=(0.01, 10000), xscale='log', yscale='log')
-ax.set_xlabel(r'$x$')
-ax.set_ylabel(r'$y(x)$')
-ax.legend()
+spl.Axes(ax, xlim=(0.1, 100), ylim=(0.01, 10000), xscale='log', yscale='log',
+         xlabel=r'$x$', ylabel=r'$y(x)$')
+
+ax.legend(frameon=False)
+
 plt.show()
